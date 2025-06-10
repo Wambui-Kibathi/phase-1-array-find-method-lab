708a33b4a99fd07c2129d689e32736e5c1e66f01
Wambui Kibathi <zeldabobo02@gmail.com>
2025-06-10 11:55:51 +0300

    Changes in the index.js file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(list) {
+    const win = list.find(function(game){
+        return game.result === "W";
+    });
+    return win ? win.year : undefined;
+}
+superbowlWin.find(findResult)
+
+function findResult(object){
+    if(object.result === "W")
+        console.log(year)
+}
